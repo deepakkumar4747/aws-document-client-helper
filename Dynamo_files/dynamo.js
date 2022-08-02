bd62c5d56feb273174c89e2dfcf401d8edd562f0
unknown <deepakofficial47@gmail.com>
2022-08-02 15:15:07 +0530

    ghj

--- Dynamo_files/dynamo.js
+++ Dynamo_files/dynamo.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk')
 require('dotenv').config()
-console.log(process.env)
+
 AWS.config.update({'region':'ap-southeast-1'})
 const Dynamo=new AWS.DynamoDB.DocumentClient({
     accessKeyId: process.env.accessKeyId, 
